7cbdae256bd4d8f80aa41bc938e2ff8163b00637
Sarah Nnanyelugo <sarahnnanyelugo@gmail.com>
2024-06-20 01:21:31 +0100

    schools table

--- src/Data/schoolsData.js
+++ src/Data/schoolsData.js
@@ -11,7 +11,7 @@ export const schools = [
     bg2: "#AA9C75",
     category: ["pending", "NE"],
     about:
-      "British Spring College was established in October 2011 by Lady Obianuju Dike. It is a co-educational full-boarding international secondary school that is approved by the British Council and the Federal /State Ministry of Education. We strongly believe that the 21st Century child should be educated with all necessary skills: moral and mental skills. The school is located at Awka, the state capital of Anambra State and it is amongst the best schools in Nigeria.",
+      "Founded in 1975 by Sir Leonard St. Crigs, St. Crigs High School has long been a beacon of academic excellence and integrity. Nestled in the heart of the city, the school offers a broad curriculum that balances rigorous academic standards with extensive extracurricular programs. Over the decades, St. Crigs has cultivated a tradition of excellence, producing graduates who excel in various fields such as medicine, law, and engineering. The school's state-of-the-art facilities, including advanced science laboratories, a comprehensive library, and modern sports amenities, provide students with the resources they need to thrive. The dedicated faculty at St. Crigs High School are committed to fostering a supportive and challenging learning environment that encourages students to reach their full potential..",
     vision:
       "Our vision is to raise a generation of students with the right tools, skills, moral and mental abilities to excel in life and contribute positively to the development of mankind. In fulfilling our mission, we seek to achieve the following:",
     mission:
@@ -19,7 +19,7 @@ export const schools = [
   },
   {
     id: 2,
-    name: "St. Crigs High School",
+    name: "Sultan High School",
     status: "Active",
     bg: "#ECFDF3",
     colo: "#027A48",
@@ -29,7 +29,7 @@ export const schools = [
     bg2: "#2A86D3",
     category: ["active", "NE"],
     about:
-      "British Spring College was established in October 2011 by Lady Obianuju Dike. It is a co-educational full-boarding international secondary school that is approved by the British Council and the Federal /State Ministry of Education. We strongly believe that the 21st Century child should be educated with all necessary skills: moral and mental skills. The school is located at Awka, the state capital of Anambra State and it is amongst the best schools in Nigeria.",
+      "Established in 1982 by renowned philanthropist Amina Sultan, Sultan High School is celebrated for its strong emphasis on cultural diversity and inclusivity. Situated in a multicultural neighborhood, the school attracts students from various backgrounds, fostering a rich tapestry of perspectives and experiences. Sultan High School's curriculum is designed to promote global awareness and intercultural understanding, with special programs in world languages, international relations, and cultural studies. The school also offers robust arts and sports programs, ensuring a well-rounded education. With its committed staff and vibrant student body, Sultan High School creates an environment where every student feels valued and empowered to succeed academically and socially.",
     vision:
       "Our vision is to raise a generation of students with the right tools, skills, moral and mental abilities to excel in life and contribute positively to the development of mankind. In fulfilling our mission, we seek to achieve the following:",
     mission:
@@ -37,7 +37,7 @@ export const schools = [
   },
   {
     id: 3,
-    name: "St. Crigs High School",
+    name: "BritishSpring College",
     status: "Active",
     regID: "NAPPS-988737",
     zone: "North Central (NC)",
@@ -47,7 +47,7 @@ export const schools = [
     bg2: "#EF9995",
     category: ["active", "NC"],
     about:
-      "British Spring College was established in October 2011 by Lady Obianuju Dike. It is a co-educational full-boarding international secondary school that is approved by the British Council and the Federal /State Ministry of Education. We strongly believe that the 21st Century child should be educated with all necessary skills: moral and mental skills. The school is located at Awka, the state capital of Anambra State and it is amongst the best schools in Nigeria.",
+      "BritishSpring College, founded in 1990 by esteemed British educator Dr. William Spring, stands out for its seamless integration of British and local curricula. Located in a picturesque suburban area, the college is known for its high academic standards and comprehensive educational approach. BritishSpring College offers a wide range of subjects, including advanced science courses, humanities, and technical education, preparing students for both local and international higher education opportunities. The school boasts impressive facilities, such as fully equipped science labs, art studios, and a modern sports complex. Dr. Spring's vision of creating a dynamic learning environment that fosters critical thinking and creativity is evident in every aspect of the school.",
     vision:
       "Our vision is to raise a generation of students with the right tools, skills, moral and mental abilities to excel in life and contribute positively to the development of mankind. In fulfilling our mission, we seek to achieve the following:",
     mission:
@@ -55,7 +55,7 @@ export const schools = [
   },
   {
     id: 4,
-    name: "Sultan High School Kaduna",
+    name: "Dowen College",
     status: "Inactive",
     regID: "NAPPS-988737",
     zone: "North Central (NC)",
@@ -65,7 +65,7 @@ export const schools = [
     bg2: "#2A7918",
     category: ["inactive", "NC"],
     about:
-      "British Spring College was established in October 2011 by Lady Obianuju Dike. It is a co-educational full-boarding international secondary school that is approved by the British Council and the Federal /State Ministry of Education. We strongly believe that the 21st Century child should be educated with all necessary skills: moral and mental skills. The school is located at Awka, the state capital of Anambra State and it is amongst the best schools in Nigeria.",
+      "Launched in 1997 by visionary entrepreneur Olumide Dowen, Dowen College is synonymous with innovation and excellence. Situated in a bustling urban center, the college offers a cutting-edge curriculum that blends traditional academics with modern technology and entrepreneurship education. Dowen College is equipped with state-of-the-art facilities, including smart classrooms, a digital library, and advanced laboratories, providing students with the tools they need to excel in the 21st century. The school emphasizes practical learning experiences and industry partnerships, preparing students for real-world challenges. Under Olumide Dowen's leadership, the college has become a leader in educational innovation, producing graduates who are not only academically proficient but also entrepreneurial and tech-savvy..",
     vision:
       "Our vision is to raise a generation of students with the right tools, skills, moral and mental abilities to excel in life and contribute positively to the development of mankind. In fulfilling our mission, we seek to achieve the following:",
     mission:
@@ -73,7 +73,7 @@ export const schools = [
   },
   {
     id: 5,
-    name: "Sultan High School Kaduna",
+    name: "CherryField College",
     status: "Inactive",
     regID: "NAPPS-988737",
     zone: "South West (SW)",
@@ -83,7 +83,7 @@ export const schools = [
     bg2: "#2A7918",
     category: ["inactive", "SW"],
     about:
-      "British Spring College was established in October 2011 by Lady Obianuju Dike. It is a co-educational full-boarding international secondary school that is approved by the British Council and the Federal /State Ministry of Education. We strongly believe that the 21st Century child should be educated with all necessary skills: moral and mental skills. The school is located at Awka, the state capital of Anambra State and it is amongst the best schools in Nigeria.",
+      "CherryField College, founded by the dedicated educator Jane Cherry in 2000, prides itself on creating a nurturing and supportive learning environment. Located in a serene, leafy suburb, the college is known for its focus on student well-being and personal development. CherryField College offers a broad curriculum that includes strong academic programs, arts, and community service initiatives, ensuring a holistic education. The school's beautiful campus features modern classrooms, art studios, and recreational facilities that encourage both academic and personal growth. Jane Cherry's commitment to fostering a caring community is reflected in the school's approach, which emphasizes respect, empathy, and collaboration among students and staff.",
     vision:
       "Our vision is to raise a generation of students with the right tools, skills, moral and mental abilities to excel in life and contribute positively to the development of mankind. In fulfilling our mission, we seek to achieve the following:",
     mission:
@@ -91,7 +91,7 @@ export const schools = [
   },
   {
     id: 6,
-    name: "Sultan High School Kaduna",
+    name: "WisVille Schools Enugu",
     status: "Active",
     regID: "NAPPS-988737",
     zone: "South West (SW)",
@@ -101,7 +101,7 @@ export const schools = [
     bg2: "#2A7918",
     category: ["active", "SW"],
     about:
-      "British Spring College was established in October 2011 by Lady Obianuju Dike. It is a co-educational full-boarding international secondary school that is approved by the British Council and the Federal /State Ministry of Education. We strongly believe that the 21st Century child should be educated with all necessary skills: moral and mental skills. The school is located at Awka, the state capital of Anambra State and it is amongst the best schools in Nigeria.",
+      "Established in 2003 by Dr. Chris Wisville, WisVille Schools Enugu focuses on providing a holistic education that balances academics, character development, and extracurricular activities. Located in Enugu, the school is renowned for its innovative teaching methods and commitment to educational excellence. WisVille Schools offer a diverse curriculum that includes STEM programs, arts, and humanities, catering to a wide range of interests and aptitudes. The school's facilities are top-notch, featuring modern science labs, a comprehensive library, and extensive sports facilities. Dr. Wisville's vision of nurturing well-rounded individuals is evident in the school's emphasis on leadership, community service, and personal growth.",
     vision:
       "Our vision is to raise a generation of students with the right tools, skills, moral and mental abilities to excel in life and contribute positively to the development of mankind. In fulfilling our mission, we seek to achieve the following:",
     mission:
@@ -109,7 +109,7 @@ export const schools = [
   },
   {
     id: 7,
-    name: "Sultan High School Kaduna",
+    name: "Hilton Top HighSchool",
     status: "Active",
     regID: "NAPPS-988737",
     zone: "North West (NW)",
@@ -119,15 +119,15 @@ export const schools = [
     bg2: "#2A7918",
     category: ["active", "WN"],
     about:
-      "British Spring College was established in October 2011 by Lady Obianuju Dike. It is a co-educational full-boarding international secondary school that is approved by the British Council and the Federal /State Ministry of Education. We strongly believe that the 21st Century child should be educated with all necessary skills: moral and mental skills. The school is located at Awka, the state capital of Anambra State and it is amongst the best schools in Nigeria.",
+      "Founded by Margaret Hilton in 2005, Hilton Top HighSchool is renowned for its rigorous academic programs and commitment to student success. Situated in a vibrant community, the school offers a challenging curriculum that prepares students for higher education and future careers. Hilton Top HighSchool boasts impressive facilities, including advanced science and computer labs, a well-stocked library, and extensive sports amenities. The school's dedicated faculty and staff work tirelessly to create a supportive and motivating learning environment. Margaret Hilton's vision of excellence is reflected in the school's focus on high academic standards, personal integrity, and community involvement, making it a top choice for families seeking quality education.",
     vision:
       "Our vision is to raise a generation of students with the right tools, skills, moral and mental abilities to excel in life and contribute positively to the development of mankind. In fulfilling our mission, we seek to achieve the following:",
     mission:
       "Impart high quality education to students and develop inquiring minds and a desire for knowledge. Develop strong self-esteem and high personal expectation, and acquire a set of spiritual and moral values. Equip students with the tools to excel in life and skills to manage changes. Provide conducive environment for learning, and expose students to the rich and diverse culture of the environment and Nigeria in general. Impart leadership skills.",
   },
   {
-    id: 8,
-    name: "Sultan High School Kaduna",
+    id: 9,
+    name: "Montessori International School",
     status: "Pending",
     regID: "NAPPS-988737",
     zone: "South South (SS)",
@@ -137,7 +137,25 @@ export const schools = [
     bg2: "#2A7918",
     category: ["pending", "SS"],
     about:
-      "British Spring College was established in October 2011 by Lady Obianuju Dike. It is a co-educational full-boarding international secondary school that is approved by the British Council and the Federal /State Ministry of Education. We strongly believe that the 21st Century child should be educated with all necessary skills: moral and mental skills. The school is located at Awka, the state capital of Anambra State and it is amongst the best schools in Nigeria.",
+      "Founded by Maria T. Montessori in 2015, Montessori International School offers a unique, child-centered approach to education based on the Montessori philosophy. Located in a peaceful residential area, the school is designed to foster independence, creativity, and a love for learning. Montessori International School features specially prepared classrooms with hands-on learning materials that encourage exploration and discovery. The school's curriculum covers a wide range of subjects, including language arts, mathematics, science, and cultural studies, all taught through interactive and engaging methods. Maria T. Montessori's vision of an educational environment that respects and nurtures each child's individual potential is at the heart of the school's philosophy and practices.",
+    vision:
+      "Our vision is to raise a generation of students with the right tools, skills, moral and mental abilities to excel in life and contribute positively to the development of mankind. In fulfilling our mission, we seek to achieve the following:",
+    mission:
+      "Impart high quality education to students and develop inquiring minds and a desire for knowledge. Develop strong self-esteem and high personal expectation, and acquire a set of spiritual and moral values. Equip students with the tools to excel in life and skills to manage changes. Provide conducive environment for learning, and expose students to the rich and diverse culture of the environment and Nigeria in general. Impart leadership skills.",
+  },
+  {
+    id: 8,
+    name: "Norwegian College",
+    status: "Pending",
+    regID: "NAPPS-988737",
+    zone: "South South (SS)",
+    bg: "#F9FAFB",
+    colo: "#344054",
+    alphabet: "S",
+    bg2: "#2A7918",
+    category: ["pending", "SS"],
+    about:
+      "Opened in 2010 by Dr. Erik Nilsen, Norwegian College brings Scandinavian educational principles to the local context, offering a unique approach to learning. Located in a tranquil setting, the college emphasizes experiential learning, critical thinking, and environmental awareness. Norwegian College's curriculum includes strong academic programs alongside innovative courses in sustainability, outdoor education, and global citizenship. The school's campus features eco-friendly buildings, outdoor classrooms, and state-of-the-art laboratories, providing an ideal environment for holistic education. Dr. Nilsen's dedication to fostering a love for learning and a sense of global responsibility is evident in every aspect of Norwegian College's approach.",
     vision:
       "Our vision is to raise a generation of students with the right tools, skills, moral and mental abilities to excel in life and contribute positively to the development of mankind. In fulfilling our mission, we seek to achieve the following:",
     mission:
