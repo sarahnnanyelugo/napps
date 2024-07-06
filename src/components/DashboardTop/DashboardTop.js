46df36ceb960863d35136e5258353f4eaa640bf3
Sarah Nnanyelugo <sarahnnanyelugo@gmail.com>
2024-07-06 19:56:48 +0100

    uploads

--- src/components/DashboardTop/DashboardTop.js
+++ src/components/DashboardTop/DashboardTop.js
@@ -7,7 +7,7 @@ export const DashboardTop = (props) => {
   return (
     <>
       <div className="d-flex top-navv ">
-        <h1 style={{ flexGrow: 1 }}>{props.title}</h1>
+        <h2 style={{ flexGrow: 1 }}>{props.title}</h2>
         <input placeholder="Search" className="col-md-3" />
         <div style={{ marginLeft: "16px", marginRight: "16px" }}>
           <img src={Icon3} width="24px" />
